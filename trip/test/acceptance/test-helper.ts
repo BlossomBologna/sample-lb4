43dc1c0c620926f92e4be4dc361e37f4e6e016f4
Chip Salim <chipsalim@onoff.insure>
2018-12-13 16:59:01 +0700

    Initial project structure

---
+++ trip/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {TripApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new TripApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: TripApplication;
+  client: Client;
+}
